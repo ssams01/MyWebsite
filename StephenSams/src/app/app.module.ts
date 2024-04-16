c7e79420c8db048663e949df5563cfb53fec27f2
ssams01 <airjordanfranchize5@gmail.com>
2024-04-16 17:08:39 -0400

    created routing file for later navbar use, and created & rendered home component

--- StephenSams/src/app/app.module.ts
+++ StephenSams/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
